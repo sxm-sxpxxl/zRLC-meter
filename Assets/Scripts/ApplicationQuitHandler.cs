e1651199424c0b735d743a2205fbee1b14ba7f0d
sxm-sxpxxl <alescha.pestov@yandex.ru>
2022-04-21 06:14:18 +0700

    Добавлена кнопка закрытия приложения на главный экран.

---
+++ Assets/Scripts/ApplicationQuitHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public sealed class ApplicationQuitHandler : MonoBehaviour
+{
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
