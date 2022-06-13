ca80fe1e7bc5a136a376391b11e26caa9eeda921
sxm-sxpxxl <alescha.pestov@yandex.ru>
2022-06-14 00:54:41 +0700

    Импорт контента. Реализован Tab Group

---
+++ Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,114 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(Image)), DisallowMultipleComponent, ExecuteInEditMode]
+public sealed class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+{
+    public event Action<TabButton> OnClick = delegate { };
+    
+    [SerializeField] private State defaultState = State.Idle;
+
+    [Header("Content")]
+    [SerializeField] private Image icon;
+    [SerializeField] private TMP_Text label;
+    [SerializeField] private Image underscore;
+
+    [Header("States config")]
+    [SerializeField] private Color idleColor;
+    [SerializeField] private Color hoverColor;
+    [SerializeField] private Color pressColor;
+    [SerializeField] private Color disableColor;
+    [SerializeField] private Color selectColor;
+
+    [Space]
+    [SerializeField] private TMP_FontAsset idleFontAsset;
+    [SerializeField] private TMP_FontAsset selectFontAsset;
+
+    private State _currentState;
+    
+    public enum State
+    {
+        Idle,
+        Hover,
+        Press,
+        Disable,
+        Select
+    }
+
+    private void OnValidate()
+    {
+        SetState(defaultState);
+    }
+
+    private void Awake()
+    {
+        SetState(defaultState);
+    }
+
+    public void SetState(State state)
+    {
+        Color stateColor = GetStateColor(state);
+
+        icon.color = stateColor;
+        label.color = stateColor;
+        underscore.color = stateColor;
+        
+        underscore.gameObject.SetActive(state == State.Select);
+        label.font = state == State.Select ? selectFontAsset : idleFontAsset;
+        
+        _currentState = state;
+    }
+    
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        if (_currentState == State.Idle)
+        {
+            SetState(State.Hover);
+        }
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        if (_currentState == State.Hover)
+        {
+            SetState(State.Idle);
+        }
+    }
+    
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        if (_currentState == State.Hover)
+        {
+            SetState(State.Press);
+        }
+    }
+    
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        if (_currentState == State.Press)
+        {
+            SetState(State.Hover);
+        }
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        if (_currentState != State.Select)
+        {
+            OnClick.Invoke(this);
+        }
+    }
+
+    private Color GetStateColor(State state) => state switch
+    {
+        State.Idle => idleColor,
+        State.Hover => hoverColor,
+        State.Press => pressColor,
+        State.Disable => disableColor,
+        State.Select => selectColor,
+        _ => throw new ArgumentOutOfRangeException(nameof(state), state, null)
+    };
+}
---
+++ Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[DisallowMultipleComponent]
+public sealed class TabGroup : MonoBehaviour
+{
+    [SerializeField] private List<TabPage> tabPages = new List<TabPage>();
+    [SerializeField, Range(0, 3)] private int defaultTabPageIndex = 0;
+
+    private TabPage _selectedTabPage;
+    
+    [Serializable]
+    private class TabPage
+    {
+        public TabButton tab;
+        public GameObject page;
+    }
+
+    private void OnValidate()
+    {
+        SetTabPage(defaultTabPageIndex);
+    }
+
+    private void Awake()
+    {
+        foreach (var tabPage in tabPages)
+        {
+            tabPage.tab.OnClick += OnTabClick;
+        }
+    }
+
+    private void Start()
+    {
+        SetTabPage(defaultTabPageIndex);
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var tabPage in tabPages)
+        {
+            tabPage.tab.OnClick -= OnTabClick;
+        }
+    }
+
+    private void OnTabClick(TabButton clickedTab)
+    {
+        if (_selectedTabPage.tab == clickedTab)
+        {
+            return;
+        }
+
+        int targetIndex = tabPages.FindIndex(0, x => x.tab == clickedTab);
+        SetTabPage(targetIndex);
+    }
+
+    private void SetTabPage(int index)
+    {
+        Assert.IsTrue(index >= 0 && index < tabPages.Count, "Tab page index doesn't match known tab pages.");
+
+        if (_selectedTabPage != null)
+        {
+            _selectedTabPage.tab.SetState(TabButton.State.Idle);
+            _selectedTabPage.page.SetActive(false);
+        }
+        
+        tabPages[index].tab.SetState(TabButton.State.Select);
+        tabPages[index].page.SetActive(true);
+        
+        _selectedTabPage = tabPages[index];
+    }
+}
