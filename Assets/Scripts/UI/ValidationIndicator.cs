db84c0ce212959cd7e54976202dc405adb71d7ac
sxm-sxpxxl <alescha.pestov@yandex.ru>
2022-06-14 20:38:42 +0700

    Добавлена первоначальная верстка основных экранов.

---
+++ Assets/Scripts/UI/ValidationIndicator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image)), DisallowMultipleComponent, ExecuteInEditMode]
+public sealed class ValidationIndicator : MonoBehaviour
+{
+    [SerializeField] private StatusData[] statuses;
+    [SerializeField] private Status defaultStatus;
+
+    private Image _target;
+    
+    [Serializable]
+    private struct StatusData
+    {
+        public Status status;
+        public Sprite icon;
+        public Color color;
+    }
+
+    private enum Status
+    {
+        Success,
+        Warning,
+        Error
+    }
+
+    private void OnValidate()
+    {
+        _target = GetComponent<Image>();
+        SetStatus(defaultStatus);
+    }
+
+    private void Awake()
+    {
+        _target = GetComponent<Image>();
+    }
+
+    private void SetStatus(Status status)
+    {
+        Assert.IsTrue(statuses.Any(x => x.status == status), "The status doesn't match known statuses.");
+        StatusData data = statuses.First(x => x.status == status);
+        
+        _target.sprite = data.icon;
+        _target.color = data.color;
+    }
+}
