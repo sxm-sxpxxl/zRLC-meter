2dfaca59a79cc0ecef8da366223c812580beaa76
sxm-sxpxxl <alescha.pestov@yandex.ru>
2022-06-16 18:26:24 +0700

    Исправлен баг с некорректным отображением префикса значения в InputField элементе.

---
+++ Assets/Scripts/UI/CustomRaycaster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image)), DisallowMultipleComponent]
+public sealed class CustomRaycaster : MonoBehaviour, IPointerClickHandler
+{
+    public event Action<PointerEventData> OnPointerClick = delegate { };
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        OnPointerClick.Invoke(eventData);
+    }
+}
--- Assets/Scripts/UI/InputFieldController.cs
+++ Assets/Scripts/UI/InputFieldController.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
-using TMPro;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using TMPro;
 
 [DisallowMultipleComponent]
 public sealed class InputFieldController : MonoBehaviour
 {
     [SerializeField] private TMP_InputField selfInputField;
+    [SerializeField] private CustomRaycaster customRaycaster;
     [SerializeField] private TMP_Text prefixText;
 
     [Space]
+    [SerializeField] private bool isReadonly = false;
     [SerializeField] private string defaultValue = string.Empty;
+    [Space]
+    [SerializeField] private bool isFixedUnits = false;
     [SerializeField] private string units = "Ohm";
 
     public float Value { get; private set; }
@@ -22,17 +27,19 @@ public sealed class InputFieldController : MonoBehaviour
             Debug.LogWarning($"Assign {nameof(selfInputField)} and {nameof(prefixText)} refs to update the units!");
             return;
         }
-        
+
         ResetValue();
     }
 
     private void Awake()
     {
+        customRaycaster.OnPointerClick += OnInputFieldClick;
         selfInputField.onEndEdit.AddListener(OnTextEndEdit);
     }
 
     private void OnDestroy()
     {
+        customRaycaster.OnPointerClick -= OnInputFieldClick;
         selfInputField.onEndEdit.RemoveListener(OnTextEndEdit);
     }
 
@@ -44,21 +51,49 @@ public sealed class InputFieldController : MonoBehaviour
     public void SetValue(float value)
     {
         Value = value;
+
+        if (isFixedUnits)
+        {
+            selfInputField.SetTextWithoutNotify($"{value:F1}");
+            prefixText.SetText($"{units}");
+            return;
+        }
         
         (float convertedValue, string prefix) = value.AutoConvertNormalValue();
 
-        selfInputField.SetTextWithoutNotify($"{convertedValue:N}");
+        selfInputField.SetTextWithoutNotify($"{convertedValue:F1}");
         prefixText.SetText($"{prefix}{units}");
     }
 
     public void ResetValue()
     {
         selfInputField.text = defaultValue;
+        selfInputField.readOnly = isReadonly;
         prefixText.text = $"{units}";
+    }
+    
+    private void OnInputFieldClick(PointerEventData eventData)
+    {
+        if (isReadonly)
+        {
+            return;
+        }
+        
+        string valueText = $"{Value}";
+        
+        selfInputField.SetTextWithoutNotify(valueText);
+        prefixText.SetText($"{units}");
+
+        selfInputField.selectionStringAnchorPosition = valueText.Length;
     }
 
     private void OnTextEndEdit(string text)
     {
+        if (isReadonly)
+        {
+            return;
+        }
+        
         SetValue(float.Parse(text));
         OnValueEndEdit.Invoke(Value);
     }
